ca3b957c9970cb1b81eadcad2d97d7e243c2fff3
Roshan Acharya <roshanacharya207@gmail.com>
2024-01-16 09:37:32 +0545

    weatherAPI

--- 9. Weather/script.js
+++ 9. Weather/script.js
@@ -1,6 +1,21 @@
 const button = document.getElementById("search");
 const input = document.getElementById("city");
-button.addEventListener("click", () => {
-  alert("clicked");
-  console.log(input.value);
+
+const cityName = document.getElementById("location");
+const cityTime = document.getElementById("city-time");
+const cityTemp = document.getElementById("city-temp");
+async function getData(cityName) {
+  const promise = await fetch(`
+  http://api.weatherapi.com/v1/current.json?key=c5b31f51d7d74348bb124939241001&q=${cityName}&aqi=yes`);
+  return await promise.json();
+}
+button.addEventListener("click", async () => {
+  // alert("clicked");
+  // console.log(input.value);
+  const value = input.value;
+  const result = await getData(value);
+  // console.log(result);
+  cityName.innerText = `${result.location.name},${result.location.region}- ${result.location.country}`;
+  cityTime.innerText = `${result.location.localtime}`;
+  cityTemp.innerText = `${result.current.temp_c}`;
 });
