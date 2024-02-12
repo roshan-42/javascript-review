7df42f5436cc2a7df46ec6ce02918dc6bcbe5c30
Roshan Acharya <roshanacharya207@gmail.com>
2024-02-12 11:58:45 +0545

    variable:

---
+++ 1. Variable practice/Broadlink.js
@@ -0,0 +1,11 @@
+let a = 10;
+console.log("The value of a is", a);
+
+var b = 5;
+console.log("The value of b is", b);
+var b = 13;
+console.log("the value of c is", c);
+
+var name = "hello";
+var name = "world";
+//but "let" won't allow to do this.
